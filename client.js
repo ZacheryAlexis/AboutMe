a28c0a6584acb140b98f833afecf0734e9d951f8
Zachery Alexis <zacheryalexis477@gmail.com>
2020-08-06 16:54:05 -0400

    update

--- client.js
+++ client.js
@@ -1,5 +1,16 @@
+/*
 let url = 'http://localhost:3000/Zachery';
 
+fetch(url)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+*/
+
+let url = 'https://accountdatapractice.herokuapp.com/Cloud';
 fetch(url)
     .then(function(res){
         return res.json();
